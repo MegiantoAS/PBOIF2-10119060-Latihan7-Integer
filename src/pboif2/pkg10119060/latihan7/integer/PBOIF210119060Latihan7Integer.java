5e2243f5d49b686429be4d3dfe6ef79239f97eaf
asus <asus@LAPTOP-BD2P4KVT>
2020-10-15 19:55:55 +0700

    new commit

---
+++ src/pboif2/pkg10119060/latihan7/integer/PBOIF210119060Latihan7Integer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119060.latihan7.integer;
+
+/**
+ *
+  * @author Megianto Adi saputra
+ * Nama         : Megianto Adi Saputra
+ * Kelas        : IF-2
+ * NIM          : 10119060
+ * Deskripsi    : integer
+ */
+public class PBOIF210119060Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i =1_000_000_000;
+        l =9000000000L;
+        
+        System.out.println("byte : "+ b);
+        System.out.println("short : "+ s);
+        System.out.println("int : "+ i);
+        System.out.println("log : "+ l);
+    }
+    
+}
